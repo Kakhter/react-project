7d4e19cff6271ed60c949780c55e787df596c438
Kakhter <khalid.bharat@gmail.com>
2025-10-11 13:37:40 +0530

    clean

--- src/Component/personal/TVSPaper.jsx
+++ src/Component/personal/TVSPaper.jsx
@@ -4,7 +4,9 @@ import styles from "../../style/tvsStyle.module.css";
 const TVSPaper = () => {
   return (
     <>
-      <h3 className={styles.box}>Transport Department</h3>
+      <h3 className={styles.box}>
+        Transport Department E Drive - Target - gyansysCopy folder
+      </h3>
       <hr />
       <img src="./Vehicle/TransportDepartment.jpeg" width="90%"></img>
       <br></br>
