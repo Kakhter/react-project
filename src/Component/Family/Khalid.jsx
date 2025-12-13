0c4c3718e3bebfa8388a030f647464a6b327be7a
kakhter <khalid.bharat@gmail.com>
2025-12-13 20:27:53 +0530

    photo marks details added

--- src/Component/Family/Khalid.jsx
+++ src/Component/Family/Khalid.jsx
@@ -11,8 +11,14 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const Khalid = () => {
   return (
     <>
-    <div>Hello Khalid</div>
-     <br/>
+    <div>Khalid Ids</div>
+    <br/>
+     <label>Photo</label><br/>
+      <img src="/KhalidIDs/PHOTO.jpg" width="20%"></img>
+     
+    <br/>
+
+
      <label>Khalid Aadhar</label><br/>
      
     <img src="/KhalidIDs/AadharI.jpeg" width="20%"></img>
@@ -41,8 +47,69 @@ const Khalid = () => {
     <br/>
      <label>Voter</label><br/>
       <img src="/KhalidIDs/VoterI.jpeg" width="20%"></img>
-    <img src="/KhalidIDs/VoterII.jpeg" width="20%"></img>
+     <img src="/KhalidIDs/VoterII.jpeg" width="20%"></img>
     <br/>
+
+ 
+  <table style={{ width: "100%", borderCollapse: "collapse" }}>
+      <thead>
+        <tr>
+          <th style={{ border: "1px solid black", padding: "8px" }}>Sl. No.</th>
+          <th style={{ border: "1px solid black", padding: "8px" }}>Examination</th>
+          <th style={{ border: "1px solid black", padding: "8px" }}>Board / University</th>
+          <th style={{ border: "1px solid black", padding: "8px" }}>Year</th>
+          <th style={{ border: "1px solid black", padding: "8px" }}>% Obtained</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td style={{ border: "1px solid black", padding: "8px" }}>1.</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>
+            MCA (Master of Computer Application)
+          </td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>
+            University of North Bengal (W.B)
+          </td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>2000</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>75.42 %</td>
+        </tr>
+        <tr>
+          <td style={{ border: "1px solid black", padding: "8px" }}>2.</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>
+            Master of Science (Electronic)
+          </td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>BNMU (Madhepura)</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>1995</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>65.62 %</td>
+        </tr>
+        <tr>
+          <td style={{ border: "1px solid black", padding: "8px" }}>3.</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>
+            Bachelor of Science (Physics, Hons)
+          </td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>BNMU (Madhepura)</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>1991</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>64.26 %</td>
+        </tr>
+        <tr>
+          <td style={{ border: "1px solid black", padding: "8px" }}>4.</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>Higher Secondary</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>
+            B.I.E. Council (Patna)
+          </td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>88</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>48.11 %</td>
+        </tr>
+        <tr>
+          <td style={{ border: "1px solid black", padding: "8px" }}>5.</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>Secondary</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>B.S.E.B. (Patna)</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>86</td>
+          <td style={{ border: "1px solid black", padding: "8px" }}>57.22 %</td>
+        </tr>
+      </tbody>
+    </table>
+
     <br/>
     <br/>
 
